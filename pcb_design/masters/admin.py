bfa2765fda75430972caf5b4d0276c64679e8929
Nandavaram-Mahesh <mahesh.nandavaram96@gmail.com>
2025-01-06 15:43:17 +0530

    Masters(Admin.site.register) Changes

--- pcb_design/masters/admin.py
+++ pcb_design/masters/admin.py
@@ -1,4 +1,10 @@
 from django.contrib import admin
 from .models import MstComponent, MstCategory, MstSubCategory, MstSectionRules, MstDesignOptions, MstSubCategoryRelations, MstSubCategoryTwo
 # Register your models here.
-admin.site.register((MstComponent, MstCategory, MstSubCategory, MstSectionRules, MstDesignOptions,MstSubCategoryRelations, MstSubCategoryTwo))
+admin.site.register(MstComponent)
+admin.site.register(MstCategory)
+admin.site.register(MstSubCategory)
+admin.site.register(MstSectionRules)
+admin.site.register(MstDesignOptions)
+admin.site.register(MstSubCategoryRelations)
+admin.site.register(MstSubCategoryTwo)
