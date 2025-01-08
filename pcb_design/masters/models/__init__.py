c490aae4612d2cda812194a6f0448f7685ba5fc9
Nandavaram-Mahesh <mahesh.nandavaram96@gmail.com>
2025-01-08 17:25:36 +0530

    changes

--- pcb_design/masters/models/__init__.py
+++ pcb_design/masters/models/__init__.py
@@ -3,5 +3,5 @@ from .MstSectionGroupings import MstSectionGroupings
 from .MstCategory import MstCategory
 from .MstSubCategory import MstSubCategory
 from .MstComponet import MstComponent
-from .BaseModel import BaseModel
+# from .BaseModel import BaseModel
 from .MstSubCategoryTwo import MstSubCategoryTwo
