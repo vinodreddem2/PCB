de0ce9fa2ce5f90b8fa0a119b669462f3e1e8706
Nandavaram-Mahesh <mahesh.nandavaram96@gmail.com>
2025-01-08 17:34:47 +0530

    admin panel changes (removed created_by)

--- pcb_design/masters/admin.py
+++ pcb_design/masters/admin.py
@@ -3,39 +3,39 @@ from .models import MstComponent, MstCategory, MstSubCategory, MstSectionRules, 
 
 
 class MstComponentAdmin(admin.ModelAdmin):
-    list_display = ('id', 'component_name', 'description', 'created_by')  
+    list_display = ('id', 'component_name', 'description')  
     search_fields = ('component_name', 'description')  
-    list_filter = ('component_name', 'created_by',)  
+    list_filter = ('component_name',)  
 
 
 class MstCategoryAdmin(admin.ModelAdmin):
-    list_display = ('id', 'category_name', 'component_Id', 'created_by')  
+    list_display = ('id', 'category_name', 'component_Id' )  
     search_fields = ('category_name', 'component_Id__component_name')  
-    list_filter = ('category_name', 'component_Id', 'created_by')  
+    list_filter = ('category_name', 'component_Id')  
 
 
 class MstSubCategoryAdmin(admin.ModelAdmin):
-    list_display = ('id', 'sub_category_name', 'category_Id', 'created_by')  
+    list_display = ('id', 'sub_category_name', 'category_Id' )  
     search_fields = ('sub_category_name', 'category_Id__category_name')  
-    list_filter = ('sub_category_name', 'created_by')  
+    list_filter = ('sub_category_name',)  
 
 
 class MstSectionRulesAdmin(admin.ModelAdmin):
-    list_display = ('id', 'rule_number', 'parameter', 'min_value', 'max_value', 'nominal', 'created_by')  
+    list_display = ('id', 'rule_number', 'parameter', 'min_value', 'max_value', 'nominal' )  
     search_fields = ('rule_number', 'parameter')  
-    list_filter = ('rule_number', 'created_by',)  
+    list_filter = ('rule_number' ,)  
 
 
 class MstSectionGroupingsAdmin(admin.ModelAdmin):
-    list_display = ('id', 'design_doc', 'design_name', 'created_by')  
+    list_display = ('id', 'design_doc', 'design_name' )  
     search_fields = ('design_doc', 'design_name')  
-    list_filter = ('design_name', 'created_by',)   
+    list_filter = ('design_name' ,)   
 
 
 class MstSubCategoryTwoAdmin(admin.ModelAdmin):
-    list_display = ('id', 'sub_2_category_name', 'sub_category_id', 'created_by')  
+    list_display = ('id', 'sub_2_category_name', 'sub_category_id' )  
     search_fields = ('sub_2_category_name', 'sub_category_id__sub_category_name')  
-    list_filter = ('sub_2_category_name', 'created_by')  
+    list_filter = ('sub_2_category_name',)  
 
 
 admin.site.register(MstComponent, MstComponentAdmin)
